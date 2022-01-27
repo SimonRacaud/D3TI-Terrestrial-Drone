0ca83f6db8b2b29495aeb9f19945fe6f7f932c98
simon.racaud <simon.racaud@epitech.eu>
2022-01-27 17:10:18 +0100

    feat(client/mobile): add webview to the video player

--- D3TI-client/src/VideoPlayer.tsx
+++ D3TI-client/src/VideoPlayer.tsx
@@ -6,15 +6,18 @@ export default class VideoPlayer extends React.Component {
 
     render() {
         return (
+
             <View style={styles.container}>
                 <WebView
+                    style={styles.webview}
                     originWhitelist={['*']}
                     source={{ html: '<h1>Hello world</h1>' }}
+                    onError={(event) => alert(`Webview error: ${event.nativeEvent.description}`)}
                 />
-                {<Image
+                <Image
                     source={require("../assets/camera_frame_5.png")}
                     style={styles.camera_frame}
-                />}
+                />
             </View>
         );
     }
@@ -22,11 +25,10 @@ export default class VideoPlayer extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         width: '100%',
         height: '100%',
+        flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
     },
     camera_frame: {
         width: '90%',
@@ -37,8 +39,8 @@ const styles = StyleSheet.create({
         left: '5%',
     },
     webview: {
-        marginTop: 100,
-        width: '50%',
-        height: '50%',
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'red'
     }
 });
