3551c00146632dde5bb3396d4f7e00d448f87b3d
simon.racaud <simon.racaud@epitech.eu>
2022-03-01 10:07:06 +0100

    infra(back-end): systemd service to start the backend automatically

--- onBoard/SETUP.sh
+++ onBoard/SETUP.sh
@@ -15,3 +15,10 @@ pip install fastapi && pip install "uvicorn[standard]"
 ## Streaming server
 #sudo dpkg -i rws_0.74.1_armhf.deb ## OLD
 # config in : /opt/rws/etc 
+
+
+## Service
+# Set d3ti.service in => /lib/systemd/system/
+sudo chmod 644 /lib/systemd/system/d3ti.service
+sudo systemctl daemon-reload
+sudo systemctl enable d3ti.service
